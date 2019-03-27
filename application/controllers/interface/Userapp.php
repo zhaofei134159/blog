e6f27c7eee44ba8ef04148eb58d02333f286519e
zhaofei <13700530350@163.com>
2019-03-27 23:57:44 +0800

    123

--- application/controllers/interface/Userapp.php
+++ application/controllers/interface/Userapp.php
@@ -22,14 +22,26 @@ class Userapp extends Home_Controller{
 	public function getuserinfo()
 	{
 		$appid = $this->wxprogramappid;
-		$sessionKey = $this->wxprogramappsecret;
+		$appsecret  = $this->wxprogramappsecret;//小程序的 app secret (在微信小程序管理后台获取)
 
 		$userInfo = $_POST['userInfo'];
 		$encryptedData = $_POST['encryptedData'];
 		$iv = $_POST['iv'];
+		$code = $_POST['code'];
+		$grant_type = "authorization_code"; //授权（必填）
+		 
+		$params = "appid=".$appid."&secret=".$appsecret."&js_code=".$code."&grant_type=".$grant_type;
+		$url = "https://api.weixin.qq.com/sns/jscode2session?".$params;
+		 
+		$res = json_decode(httpGet($url),true);
+		$sessionKey = $res['session_key'];//取出json里对应的值
+
+
+
+
 
 		$pc = new WXBizDataCrypt($appid, $sessionKey);
-		$errCode = $pc->decryptData($encryptedData, $iv, $data );
+		$errCode = $pc->decryptData($encryptedData, $iv, $data);
 
 		if ($errCode == 0) {
 		    print($data);
@@ -39,5 +51,17 @@ class Userapp extends Home_Controller{
 		
 	}
 
+
+	function httpGet($url) {
+	    $curl = curl_init();
+	    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($curl, CURLOPT_TIMEOUT, 500);
+	    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+	    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+	    curl_setopt($curl, CURLOPT_URL, $url);
+	    $res = curl_exec($curl);
+	    curl_close($curl);
+	    return $res;
+  	}
 	
 }
