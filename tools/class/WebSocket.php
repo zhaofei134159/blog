b137b5f50bd087938c5880db667514aeee67f240
zhaofei <13700530350@163.com>
2019-05-14 23:35:29 +0800

    123123

--- tools/class/WebSocket.php
+++ tools/class/WebSocket.php
@@ -74,6 +74,7 @@ class WebSocket{
 		        }else{
 		        	// $len = 0 为正常退出 -1 为已经执行了，只不过失败了
 		          	$len = socket_recv($sign,$buffer,2048,0);
+		          	socket_last_error();
 		          	$userid = $this->search($sign);
 		          	$user = $this->users[$userid];
 
