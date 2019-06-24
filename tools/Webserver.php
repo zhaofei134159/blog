08f4d11c6a4b729ac5fb13558aae2621ba0bbbff
zhaofei <13700530350@163.com>
2019-06-24 22:12:13 +0800

    1312

--- tools/Webserver.php
+++ tools/Webserver.php
@@ -54,7 +54,7 @@ function message_analysis($userid,$usermsg,$type){
 
 
   if($type=='msg'){
-      $usermsgJson = json_decode($usermsg,true);
+      $usermsgJson = json_decode(json_decode($usermsg,true),true);
       var_dump($usermsgJson);die;
       if(empty($usermsg)){
           error_log(date('Y-m-d H:i:s')."\t ".$usermsgJson['userId']." json数据为空".PHP_EOL,3,"./log/webServer.log");
