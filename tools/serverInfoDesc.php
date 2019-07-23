f2c50a85020e018eef68253c0a24d93300f7fb7c
Zhao Fei <zhaofei@51smart.com>
2019-07-23 15:07:32 +0800

    1231

--- tools/serverInfoDesc.php
+++ tools/serverInfoDesc.php
@@ -43,7 +43,7 @@ function get_used_status(){
   $tast_running = trim(trim($tast_info[1],'running'));
    
   //CPU占有量
-  $cpu_usage = trim(trim($cpu_info[0],'%Cpu(s): '),'%us');  //百分比
+  $cpu_usage = trim(trim($cpu_info[0],'%Cpu(s): '),'us');  //百分比
    
 
   //内存占有量
