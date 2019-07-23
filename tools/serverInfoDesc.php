c3c9e3832a949d0a4589308c3fa1abbcc3dce4ce
Zhao Fei <zhaofei@51smart.com>
2019-07-23 13:41:12 +0800

    1231

--- tools/serverInfoDesc.php
+++ tools/serverInfoDesc.php
@@ -20,7 +20,7 @@ $mysql = new MMysql($db_conf);
   
   
 function get_used_status(){
-  $fp = popen('top -b -n 2 | grep -E "^(Cpu|Mem|Tasks)"',"r");//获取某一时刻系统cpu和内存使用情况
+  $fp = popen('top -b -n 2 | grep -E "^(%Cpu|Mem|Tasks)"',"r");//获取某一时刻系统cpu和内存使用情况
   $rs = "";
   while(!feof($fp)){
     $rs .= fread($fp,1024);
@@ -28,7 +28,6 @@ function get_used_status(){
   pclose($fp);
 
   $sys_info = explode("\n",$rs);
-  var_dump($sys_info);die;
   $cpu_info = explode(",",$sys_info[4]);  //CPU占有量  数组
   $tast_info = explode(",",$sys_info[3]);//进程 数组
   $mem_info = explode(",",$sys_info[5]); //内存占有量 数组
@@ -36,7 +35,7 @@ function get_used_status(){
   $tast_running = trim(trim($tast_info[1],'running'));
    
   //CPU占有量
-  $cpu_usage = trim(trim($cpu_info[0],'Cpu(s): '),'%us');  //百分比
+  $cpu_usage = trim(trim($cpu_info[0],'%Cpu(s): '),'%us');  //百分比
    
 
   //内存占有量
