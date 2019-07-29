da46dbd27b18e25e9ada435890b983424737a75d
zhaofei <13700530350@163.com>
2019-07-29 22:52:55 +0800

    链接退出加返回

--- application/helpers/htmlrepair_helper.php
+++ application/helpers/htmlrepair_helper.php
@@ -1,123 +1,54 @@
 <?php 
 
 class Htmlrepair{
-  /** 
-  * 取HTML,并自动补全闭合 
-  * 
-  * param $html 
-  * 
-  * param $length 
-  * 
-  * param $end 
-  */ 
-  function subHtml($html, $length=50) { 
-    $result = ''; 
-    $tagStack = array(); 
-    $len = 0; 
-    $contents = preg_split("~(<[^>]+?>)~si", $html, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE); 
-    foreach($contents as $tag) { 
-      if (trim($tag) == "") continue; 
-      if (preg_match("~<([a-z0-9]+)[^/>]*?/>~si", $tag)) { 
-        $result .= $tag; 
-      } else if (preg_match("~</([a-z0-9]+)[^/>]*?>~si", $tag, $match)) { 
-        if ($tagStack[count($tagStack)-1] == $match[1]) { 
-          array_pop($tagStack); 
-          $result .= $tag; 
-        } 
-      } else if (preg_match("~<([a-z0-9]+)[^/>]*?>~si", $tag, $match)) { 
-        array_push($tagStack, $match[1]); 
-        $result .= $tag; 
-      } else if (preg_match("~<!--.*?-->~si", $tag)) { 
-        $result .= $tag; 
-      } else { 
-        if ($len + $this->mstrlen($tag) < $length) { 
-          $result .= $tag; 
-          $len += $this->mstrlen($tag); 
-        } else { 
-          $str = $this->msubstr($tag, 0, $length - $len + 1); 
-          $result .= $str; 
-          break; 
-        } 
-      } 
-    } 
-    while (!empty($tagStack)) { 
-      $result .= '</' . array_pop($tagStack) . '>'; 
-    } 
-    return $result; 
-  } 
-  /** 
-  * 取中文字符串 
-  * 
-  * param $string 字符串 
-  * 
-  * param $start 起始位 
-  * 
-  * param $length 长度 
-  * 
-  * param $charset 编码 
-  * 
-  * param $dot 附加字串 
-  */ 
-  function msubstr($string, $start, $length, $dot = '', $charset = 'UTF-8') { 
-    $string = str_replace(array('&', '"', '<', '>', ' '), array('&', '"', '<', '>', ' '), $string); 
-    if (strlen($string) <= $length) { 
-      return $string; 
-    } 
-    if (strtolower($charset) == 'utf-8') { 
-      $n = $tn = $noc = 0; 
-      while ($n < strlen($string)) { 
-        $t = ord($string[$n]); 
-        if ($t == 9 || $t == 10 || (32 <= $t && $t <= 126)) { 
-          $tn = 1; 
-          $n++; 
-        } elseif (194 <= $t && $t <= 223) { 
-          $tn = 2; 
-          $n += 2; 
-        } elseif (224 <= $t && $t <= 239) { 
-          $tn = 3; 
-          $n += 3; 
-        } elseif (240 <= $t && $t <= 247) { 
-          $tn = 4; 
-          $n += 4; 
-        } elseif (248 <= $t && $t <= 251) { 
-          $tn = 5; 
-          $n += 5; 
-        } elseif ($t == 252 || $t == 253) { 
-          $tn = 6; 
-          $n += 6; 
-        } else { 
-          $n++; 
-        } 
-        $noc++; 
-        if ($noc >= $length) { 
-          break; 
-        } 
-      } 
-      if ($noc > $length) { 
-        $n -= $tn; 
-      } 
-      $strcut = substr($string, 0, $n); 
-    } else { 
-      for($i = 0; $i < $length; $i++) { 
-        $strcut .= ord($string[$i]) > 127 ? $string[$i] . $string[++$i] : $string[$i]; 
-      } 
-    } 
-    return $strcut . $dot; 
-  } 
-  /** 
-  * 得字符串的长度，包括中英文。 
-  */ 
-  function mstrlen($str, $charset = 'UTF-8') { 
-    if (function_exists('mb_substr')) { 
-      $length = mb_strlen($str, $charset); 
-    } elseif (function_exists('iconv_substr')) { 
-      $length = iconv_strlen($str, $charset); 
-    } else { 
-      preg_match_all("/[\x01-\x7f]|[\xc2-\xdf][\x80-\xbf]|\xe0[\xa0-\xbf][\x80-\xbf]|[\xe1-\xef][\x80-\xbf][\x80-\xbf]|\xf0[\x90-\xbf][\x80-f][\x80-\xbf]|[\xf1-\xf7][\x80-\xbf][\x80-\xbf][\x80-\xbf]/", $text, $ar); 
-      $length = count($ar[0]); 
-    } 
-    return $length; 
-  } 
+  
+  function fix_html_tags($input, $single_tags = array()) {
+    $result = null;
+    $stack = array();//标签栈
+    $_single_tags = array('br', 'hr', 'img', 'input');//合理的单标签
+ 
+    if ($single_tags && is_array($single_tags)) {
+        $_single_tags = array_merge($_single_tags, $single_tags);
+        $_single_tags = array_map('strtolower', $_single_tags);
+        $_single_tags = array_unique($_single_tags);
+    }
+    //返回标签分隔之后的内容，包括标签本身
+    $content = preg_split('/(<[^>]+>)/si', $input, null, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+ 
+    foreach ($content as $val) {
+        //匹配未闭合的自闭合标签 如 <br> <hr> 等
+        if (preg_match('/<(\w+)[^\/]*>/si', $val, $m) && in_array(strtolower($m[1]), $_single_tags) ) {
+            $result .= "\r\n" . $val;
+        }
+        //匹配标准书写的自闭合标签，直接返回，不入栈
+        else if (preg_match('/<(\w+)[^\/]*\/>/si', $val, $m)) {
+            $result .= $val;
+        }
+        //匹配开始标签，并入栈
+        else if (preg_match('/<(\w+)[^\/]*>/si', $val, $m)) {
+            $result .= "\r\n" . str_repeat("\t", count($stack)) . $val;
+            array_push($stack,  $m[1]);
+        }
+        //匹配闭合标签，匹配前先判断当前闭合标签是栈顶标签的闭合，优先闭合栈顶标签
+        else if (preg_match('/<\/(\w+)[^\/]*>/si', $val, $m)) {
+            //出栈，多余的闭合标签直接舍弃
+            if (strtolower(end($stack)) == strtolower($m[1])) {
+                array_pop($stack);
+                $result .= $val;
+            }
+        } else {
+            $result .= $val;
+        }
+    }
+ 
+    //倒出所有栈内元素
+    while ($stack) {
+        $result .= "</".array_pop($stack).">";
+        $result .= "\r\n";
+    }
+ 
+    return $result;
+  }
 }
 // $str = "<div><table>x<tr>1s<td>测试<td>124"; 
 // echo subHtml($str); 
