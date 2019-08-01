ea17dcf7eb9bfbae05e7ecddcdc486039e3acfc1
Zhao Fei <zhaofei@51smart.com>
2019-08-01 14:35:51 +0800

    小程序二维码

--- application/views/home/public/footer.php
+++ application/views/home/public/footer.php
@@ -1,6 +1,6 @@
 
 <!-- CONTENT-WRAPPER SECTION END-->
-    <section class="footer-section">
+    <section class="footer-section" style="position: absolute; width: 100%;bottom: 0px;">
          <div class="widewrapper copyright">
                 &copy; 2015 blogfamily 京ICP备16026346号-1  更多的博客来自 <a href="http://zf.blogfamily.ren/" target="_blank" title="博客之家">blogfamily</a>
         </div>
