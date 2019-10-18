9fb779dba7ba162cc00d60fbc96c95901ff241e0
Zhao Fei <zhaofei@51smart.com>
2019-10-18 18:23:31 +0800

    12312321

--- public/home/assets/waifu-tips.js
+++ public/home/assets/waifu-tips.js
@@ -49,7 +49,7 @@ $('.waifu-tool .fui-user').click(function (){
 
 $('.waifu-tool .fui-info-circle').click(function (){
     //window.open('https://imjad.cn/archives/lab/add-dynamic-poster-girl-with-live2d-to-your-blog-02');
-    window.open('https://www.fghrsh.net/post/123.html');
+    window.open('http://blog.myfeiyou.com/home/about');
 });
 
 $('.waifu-tool .fui-cross').click(function (){
