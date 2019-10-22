627b9c3937addc2c1e7ac9e3c14110590f5b6990
Zhao Fei <zhaofei@51smart.com>
2019-10-22 16:28:04 +0800

    12321321

--- application/views/home/friend/index.php
+++ application/views/home/friend/index.php
@@ -1,6 +1,36 @@
 <?php 
 	$this->load->view('home/public/top');
 ?>
+
+<style type="text/css">
+#nav{width:980px;height:350px;margin:100px auto;}
+#nav ul li{list-style:none;background:rgba(0,0,0,.5);height:105px;width:180px;float:left;margin:30px 5px;position:relative;}
+#nav ul li:before{
+	content:"";height:105px;width:180px;background:rgba(0,0,0,.5);position:absolute;top:0px;left:0px;
+	transform:rotate(60deg);
+	-webkit-transform:rotate(60deg);
+	-moz-transform:rotate(60deg);
+}
+#nav ul li:after{
+	content:"";height:105px;width:180px;background:rgba(0,0,0,.5);position:absolute;top:0px;left:0px;z-index:1;
+	transform:rotate(-60deg);
+	-webkit-transform:rotate(-60deg);
+	-moz-transform:rotate(-60deg);
+}
+#nav ul li.mar{margin-left:100px;}
+#nav ul li img{
+	top:0px;left:0px;right:0px;bottom:0px;margin:auto;z-index:2;position:absolute;
+	transition:1s;
+	-webkit-transition:1s;
+	-moz-transition:1s;
+}
+#nav ul li img:hover{
+	-webkit-transform:rotate(360deg) scale(1.5); 
+	-moz-transform:rotate(360deg) scale(1.5);
+	-ms-transform:rotate(360deg) scale(1.5);
+	-o-transform:rotate(360deg) scale(1.5);
+}
+</style>
 <!-- MENU SECTION END-->
 <div class="content-wrapper" style="min-height:600px;">
     <div class="container">
@@ -11,7 +41,9 @@
                     <div class="panel-heading">
                        友链
                     </div>
-                    <div class="panel-body text-center recent-users-sec">
+                    <div class="panel-body text-center recent-users-sec" id="nav">
+                        <ul>
+
                         <?php 
                             if(!empty($friends)){
                                 foreach($friends as $friend){
@@ -20,13 +52,16 @@
                                     $headimg = '/'.$friend['img'];
                                 }
                         ?>
-                            <a href="/<?=$friend['addr']?>" target="__black">
-                                <img class="img-thumbnail" src="<?=$headimg?>" width="70" title="<?=$friend['title']?>"/>
-                            </a>
+							<li>
+	                            <a href="/<?=$friend['addr']?>" target="__black">
+	                                <img class="img-thumbnail" src="<?=$headimg?>" width="70" title="<?=$friend['title']?>"/>
+	                            </a>
+							</li>
                         <?php 
                                 }
                             }
                         ?>
+                        </ul>
                     </div>
      			</div>
             </div>
