6f2fe104aab9b4796b9a439622b8f356dd9e1446
Zhao Fei <zhaofei@51smart.com>
2019-11-01 16:47:46 +0800

    123213

--- application/controllers/home/Login.php
+++ application/controllers/home/Login.php
@@ -380,7 +380,7 @@ class Login extends Home_Controller{
 
             $access_token = $token['access_token'];
 
-            $user_url = 'https://api.github.com/user?access_token='.$access_token;
+            $user_url = 'https://api.github.com/users?access_token='.$access_token;
             $header = array();
             $header[] = "Accept: application/vnd.github.v3.full+json";
             $user_info = $this->_curl_get_request($user_url,$header);
