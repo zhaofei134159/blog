b7b3d188004722bea876ef83beb16e1a377b515f
Zhao Fei <zhaofei@51smart.com>
2019-11-01 15:54:33 +0800

    123213

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ define('HOME_PUBLIC_URL', '/public/home/');
 define('ADMIN_PUBLIC_URL', '/public/backstage/');
 define('PUBLIC_URL', '/public/public/');
 
-define('HOME_URL_HTTP', 'http://blog.myfeiyou.com/home/');
+define('HOME_URL_HTTP', 'https://blog.myfeiyou.com/home/');
 define('HOME_URL', '/home/');
 define('ADMIN_URL', '/backstage/');
 
