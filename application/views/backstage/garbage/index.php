867213cbf0afff0538d71dec15d4de8730777e29
Zhao Fei <zhaofei@51smart.com>
2020-04-30 10:32:00 +0800

    垃圾分类 后台

---
+++ application/views/backstage/garbage/index.php
@@ -0,0 +1,22 @@
+<?php 
+  $this->load->view('backstage/public/top');
+?>
+
+<div id="content">
+    <div id="content-header">
+      <div id="breadcrumb">
+        <a href="<?=ADMIN_URL?>index" title="Go to Home" class="tip-bottom"><i class="icon-home"></i>首页</a>
+        <a href="<?=ADMIN_URL?>user_work/index" class="current">垃圾分类</a>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+     
+    </div>
+</div>
+<script>
+</script>
+
+<?php 
+  $this->load->view('backstage/public/footer');
+?>
--- application/views/backstage/public/top.php
+++ application/views/backstage/public/top.php
@@ -104,6 +104,13 @@
         <li><a href="<?=ADMIN_URL?>friend">友链管理</a></li>
       </ul>
     </li>
+    <li class="submenu"> 
+      <a href="#"> <i class="icon-user"></i> <span class="text">垃圾分类</span></a>
+      <ul>
+        <li><a href="<?=ADMIN_URL?>garbage">垃圾类型</a></li>
+        <li><a href="<?=ADMIN_URL?>garbage/detail">垃圾详情</a></li>
+      </ul>
+    </li>
     <!-- <li class="submenu"> <a href="#"><i class="icon icon-file"></i> <span>Addons</span> <span class="label">3</span></a>
       <ul>
         <li><a href="gallery.html">Gallery</a></li>
