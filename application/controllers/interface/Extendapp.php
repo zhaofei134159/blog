b3d2877bde622429a2a33ada69795fa7e4b7818c
Zhao Fei <zhaofei@51smart.com>
2020-06-11 16:14:36 +0800

    语音上传

--- application/controllers/interface/Extendapp.php
+++ application/controllers/interface/Extendapp.php
@@ -24,7 +24,7 @@ class Extendapp extends Home_Controller{
 		$this->load->model('zf_cate_model');
 		$this->load->model('zf_tag_model');
 		$this->load->model('zf_user_model');
-        $this->load->library('voice/aipSpeech',array($this->voiceAppId,$this->voiceAppkey,$this->voiceSecretkey));
+        $this->load->library('voice/AipSpeech',array($this->voiceAppId,$this->voiceAppkey,$this->voiceSecretkey));
 	}
 
 	// 图文转换文字
