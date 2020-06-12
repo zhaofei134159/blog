1449fb52a135c5b600d78a256c57d7b13b8343cd
Zhao Fei <zhaofei@51smart.com>
2020-06-12 09:36:01 +0800

    修改

--- application/controllers/interface/Extendapp.php
+++ application/controllers/interface/Extendapp.php
@@ -74,12 +74,9 @@ class Extendapp extends Home_Controller{
 	public function voiceToWord(){
 		// $file = $_FILES['file'];
 		// $picFile = upload_file($file,'voiceToWord');
-		$picFile = 'public/public/voiceToWord/2020061115390652348.mp3';
+		$picFile = 'http://blog.myfeiyou.com/public/public/voiceToWord/2020061115390652348.mp3';
 
 		// 你的 APPID AK SK 
-		var_dump($this->voiceAppId);
-		var_dump($this->voiceAppkey);
-		var_dump($this->voiceSecretkey);
 		$word = $this->my_speech->asr(file_get_contents($picFile), 'mp3', 16000, array(
 		    'dev_pid' => 1537,
 		));
