5acaeedb1395c5842c441da768272d6138736b9c
Zhao Fei <zhaofei@51smart.com>
2020-06-15 15:31:58 +0800

    修改

--- application/controllers/interface/Extendapp.php
+++ application/controllers/interface/Extendapp.php
@@ -74,9 +74,9 @@ class Extendapp extends Home_Controller{
 	public function voiceToWord(){
 		// $file = $_FILES['file'];
 		// $voiceFile = upload_file($file,'voiceToWord');
-		$voiceFile = '/public/public/voiceToWord/2020061513515020187.wav';
-
-		$word = $this->my_speech->asr(file_get_contents(BLOGURL.$voiceFile), 'wav', 16000, array(
+		$voiceFile = BLOGURL.'/public/public/voiceToWord/2020061513515020187.wav';
+		var_dump($voiceFile);
+		$word = $this->my_speech->asr(file_get_contents($voiceFile), 'wav', 16000, array(
 		    'lan' => 'zh',
 		));
 		var_dump(file_get_contents($voiceFile));
