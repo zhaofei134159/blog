50817f40bd75311bec6cc1b253589fbc9c491de8
Zhao Fei <zhaofei@51smart.com>
2020-06-16 10:37:27 +0800

    腾讯语音

--- application/controllers/interface/Extendapp.php
+++ application/controllers/interface/Extendapp.php
@@ -111,6 +111,7 @@ class Extendapp extends Home_Controller{
 		$params['Version'] = '2019-06-14';
 		$params['Timestamp'] = time();
 		$params['Nonce'] = rand(10000000,99999999);
+		$params['SecretId'] = $this->secretId;
 		$params['ChannelNum'] = 1;
 		$params['EngineModelType'] = '16k_zh';
 		$params['ResTextFormat'] = 1;
@@ -120,7 +121,6 @@ class Extendapp extends Home_Controller{
 
 		# 生成签名
 		$params['Signature'] = $this->setSignature($params);
-		$params['SecretId'] = $this->secretId;
 		$url = 'https://asr.tencentcloudapi.com/?'.http_build_query($params);
 		echo $url.'<br>';
 
@@ -142,6 +142,69 @@ class Extendapp extends Home_Controller{
 
 		$callback = array('errorMsg'=>$word['err_msg'],'errorNo'=>'0','seccuss'=>$wordArr);
     	exit(json_encode($callback));
+	}
+
+	function asasdasdqweq(){
+		$secretId = "AKIDz8krbsJ5yKBZQpn74WFkmLPx3EXAMPLE";
+		$secretKey = "Gu5t9xGARNpq86cd98joQYCN3EXAMPLE";
+		$host = "cvm.tencentcloudapi.com";
+		$service = "cvm";
+		$version = "2017-03-12";
+		$action = "DescribeInstances";
+		$region = "ap-guangzhou";
+		// $timestamp = time();
+		$timestamp = 1551113065;
+		$algorithm = "TC3-HMAC-SHA256";
+
+		// step 1: build canonical request string
+		$httpRequestMethod = "POST";
+		$canonicalUri = "/";
+		$canonicalQueryString = "";
+		$canonicalHeaders = "content-type:application/json; charset=utf-8\n"."host:".$host."\n";
+		$signedHeaders = "content-type;host";
+		$payload = '{"Limit": 1, "Filters": [{"Values": ["\u672a\u547d\u540d"], "Name": "instance-name"}]}';
+		$hashedRequestPayload = hash("SHA256", $payload);
+		$canonicalRequest = $httpRequestMethod."\n"
+		    .$canonicalUri."\n"
+		    .$canonicalQueryString."\n"
+		    .$canonicalHeaders."\n"
+		    .$signedHeaders."\n"
+		    .$hashedRequestPayload;
+		echo $canonicalRequest.PHP_EOL;
+
+		// step 2: build string to sign
+		$date = gmdate("Y-m-d", $timestamp);
+		$credentialScope = $date."/".$service."/tc3_request";
+		$hashedCanonicalRequest = hash("SHA256", $canonicalRequest);
+		$stringToSign = $algorithm."\n"
+		    .$timestamp."\n"
+		    .$credentialScope."\n"
+		    .$hashedCanonicalRequest;
+		echo $stringToSign.PHP_EOL;
+
+		// step 3: sign string
+		$secretDate = hash_hmac("SHA256", $date, "TC3".$secretKey, true);
+		$secretService = hash_hmac("SHA256", $service, $secretDate, true);
+		$secretSigning = hash_hmac("SHA256", "tc3_request", $secretService, true);
+		$signature = hash_hmac("SHA256", $stringToSign, $secretSigning);
+		echo $signature.PHP_EOL;
+
+		// step 4: build authorization
+		$authorization = $algorithm
+		    ." Credential=".$secretId."/".$credentialScope
+		    .", SignedHeaders=content-type;host, Signature=".$signature;
+		echo $authorization.PHP_EOL;
+
+		$curl = "curl -X POST https://".$host
+		    .' -H "Authorization: '.$authorization.'"'
+		    .' -H "Content-Type: application/json; charset=utf-8"'
+		    .' -H "Host: '.$host.'"'
+		    .' -H "X-TC-Action: '.$action.'"'
+		    .' -H "X-TC-Timestamp: '.$timestamp.'"'
+		    .' -H "X-TC-Version: '.$version.'"'
+		    .' -H "X-TC-Region: '.$region.'"'
+		    ." -d '".$payload."'";
+		echo $curl.PHP_EOL;
 	}
 
 	/**
