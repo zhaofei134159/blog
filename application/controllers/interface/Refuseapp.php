d609445a88d432780dfcb262abd8db9bfc33e1f4
Zhao Fei <zhaofei@51smart.com>
2020-06-18 18:45:57 +0800

    修改

--- application/controllers/interface/Refuseapp.php
+++ application/controllers/interface/Refuseapp.php
@@ -2,7 +2,7 @@
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 date_default_timezone_set("Asia/Shanghai"); //设置时区
 
-class Extendapp extends Home_Controller{
+class Refuseapp extends Home_Controller{
 
 	# 京东
 	public $refuseAppKey = '';
@@ -24,8 +24,76 @@ class Extendapp extends Home_Controller{
 	}
 
 	public function refuseEntityDiscern(){
+		$file = $_FILES['file'];
+		$picFile = upload_img($file,'refuseImg');
+		echo $picFile;
+		die;
 
-	} 
+		# 获取毫秒时间戳
+		$timestamp = $this->getMillisecond();
 
+		$url = 'https://aiapi.jd.com/jdai/garbageImageSearch';
+		$query = array();
+		$query['appkey'] = $this->refuseAppKey;
+		$query['timestamp'] = $timestamp
+		$query['sign'] = MD5($this->refuseSecretKey.$timestamp);
+		$url .= http_build_query($query);
+
+		$param = array();
+		$param['imgBase64'] = '';
+		$param['cityId'] = '110000';
+
+		$header = array('Content-Type:application/json;charset=UTF-8');
+
+		$result = request($url,$param,$header);
+
+		// @unlink($picFile);
+
+		$callback = array('errorMsg'=>'','errorNo'=>'0','seccuss'=>$wordResArr);
+    	exit(json_encode($callback));
+	}
+
+
+	/**
+	* 发起http post请求(REST API), 并获取REST请求的结果
+	* @param string $url
+	* @param string $param
+	* @return - http response body if succeeds, else false.
+	*/
+	function request($url = '', $param = array(), $header = array())
+	{
+		if (empty($url)) {
+			return false;
+		}
+
+		$postUrl = $url;
+		$curlPost = $param;
+		// 初始化curl
+		$curl = curl_init();
+	    if(!empty($header)){
+	        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+	        curl_setopt($curl, CURLOPT_HEADER, 0);//返回response头部信息
+	    }
+		curl_setopt($curl, CURLOPT_URL, $postUrl);
+		curl_setopt($curl, CURLOPT_HEADER, 0);
+		// 要求结果为字符串且输出到屏幕上
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		// post提交方式
+		if(!empty($curlPost)){
+			curl_setopt($curl, CURLOPT_POST, 1);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $curlPost);
+		}
+		// 运行curl
+		$data = curl_exec($curl);
+		curl_close($curl);
+
+		return $data;
+	}
+
+	private function getMillisecond(){
+	    list($t1, $t2) = explode(' ', microtime());
+	    return (float)sprintf('%.0f', (floatval($t1) + floatval($t2)) * 1000);
+	}
 
 }
