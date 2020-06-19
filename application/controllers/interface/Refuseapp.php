c41733ca5a7604389e8a1f1a512550aabd490b6e
Zhao Fei <zhaofei@51smart.com>
2020-06-19 17:02:50 +0800

    修改

--- application/controllers/interface/Refuseapp.php
+++ application/controllers/interface/Refuseapp.php
@@ -82,7 +82,7 @@ class Refuseapp extends Home_Controller{
 		$propertyArr['encode']['channel'] = 1;
 		$propertyArr['encode']['format'] = 'mp3';
 		$propertyArr['encode']['sample_rate'] = '16000';
-		$propertyArr['encode']['post_process'] = '1'; # 开启后 一千 = 1000 
+		$propertyArr['encode']['post_process'] = '0'; # 开启后 一千 = 1000 
 		$propertyArr['platform'] = 'Linux';
 		$propertyArr['version'] = '0.0.0.1';
 
