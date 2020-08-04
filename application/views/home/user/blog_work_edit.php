fc8ef5640609ddc71a3fa9c77123ae8e677c2314
zhaofei <zhaofei@51smart.com>
2020-08-04 18:26:15 +0800

    123123

--- application/views/home/user/blog_work_edit.php
+++ application/views/home/user/blog_work_edit.php
@@ -6,26 +6,62 @@
 <script charset="utf-8" src="<?=PUBLIC_URL?>kindeditor/kindeditor.js"></script>
 <script charset="utf-8" src="<?=PUBLIC_URL?>kindeditor/lang/zh_CN.js"></script>
 <script charset="utf-8" src="<?=PUBLIC_URL?>kindeditor/plugins/code/prettify.js"></script>
+
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="<?=HOME_PUBLIC_URL?>editormd/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?=HOME_PUBLIC_URL?>editormd/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="<?=HOME_PUBLIC_URL?>editormd/css/editormd.css" />
+<script src="<?=HOME_PUBLIC_URL?>editormd/editormd.js"></script>
 <script>
-    KindEditor.ready(function(K) {
-        var editor1 = K.create('textarea[name="desc"]', {
-            cssPath : '<?=PUBLIC_URL?>kindeditor/plugins/code/prettify.css',
-            uploadJson : '<?=PUBLIC_URL?>kindeditor/php/upload_json.php',
-            fileManagerJson : '<?=PUBLIC_URL?>kindeditor/php/file_manager_json.php',
-            allowFileManager : true,
-            afterCreate : function() {
-                var self = this;
-                K.ctrl(document, 13, function() {
-                    self.sync();
-                    K('form[name=example]')[0].submit();
-                });
-                K.ctrl(self.edit.doc, 13, function() {
-                    self.sync();
-                    K('form[name=example]')[0].submit();
-                });
-            }
-        });
-        prettyPrint();
+    // KindEditor.ready(function(K) {
+    //     var editor1 = K.create('textarea[name="desc"]', {
+    //         cssPath : '<?=PUBLIC_URL?>kindeditor/plugins/code/prettify.css',
+    //         uploadJson : '<?=PUBLIC_URL?>kindeditor/php/upload_json.php',
+    //         fileManagerJson : '<?=PUBLIC_URL?>kindeditor/php/file_manager_json.php',
+    //         allowFileManager : true,
+    //         afterCreate : function() {
+    //             var self = this;
+    //             K.ctrl(document, 13, function() {
+    //                 self.sync();
+    //                 K('form[name=example]')[0].submit();
+    //             });
+    //             K.ctrl(self.edit.doc, 13, function() {
+    //                 self.sync();
+    //                 K('form[name=example]')[0].submit();
+    //             });
+    //         }
+    //     });
+    //     prettyPrint();
+    // });
+    var testEditor;
+
+    $(function() {     
+        
+        $.get("../examples/test.md", function(md){
+            testEditor = editormd("test-editormd", {
+                width: "90%",
+                height: 740,
+                path : '../lib/',
+                markdown : md,
+                codeFold : true,
+                saveHTMLToTextarea : true, 
+                searchReplace : true,
+                htmlDecode : "style,script,iframe|on*", 
+                emoji : true,
+                taskList : true,
+                tocm            : true,         // Using [TOCM]
+                tex : true,                   // 开启科学公式TeX语言支持，默认关闭
+                flowChart : true,             // 开启流程图支持，默认关闭
+                sequenceDiagram : true,       // 开启时序/序列图支持，默认关闭,
+                imageUpload : true,
+                imageFormats : ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
+                imageUploadURL : "../examples/php/upload.php",
+                onload : function() {
+                    console.log('onload', this);
+                }
+            });
+        });  
     });
 </script>
 <!-- MENU SECTION END-->
@@ -122,7 +158,10 @@
                                 </div>
                                 <div class="form-group  col-md-12">
                                     <label>文章内容：</label>
-                                    <textarea class="form-control col-md-12" name="desc" id="desc" style="min-height:100px;visibility:hidde" /><?=empty($work['desc'])?'':$work['desc']?></textarea>
+                                    <!-- <textarea class="form-control col-md-12" name="desc" id="desc" style="min-height:100px;visibility:hidde" /><?=empty($work['desc'])?'':$work['desc']?></textarea> -->
+                                    <div id="test-editormd">
+                                        <textarea style="display:none;">### Bootstrap 兼容测试</textarea>
+                                    </div>
                                 </div>
                                 <div class="form-group  col-md-12">
                                     <label></label>
