dc5523d7c96756787b394ac13df837a9d9f5324f
zhaofei <zhaofei@51smart.com>
2020-08-04 18:42:49 +0800

    123123

--- public/home/editormd/editormd.js
+++ public/home/editormd/editormd.js
@@ -100,7 +100,7 @@
         width                : "100%",
         height               : "100%",
         path                 : "./lib/",       // Dependents module file directory
-        pluginPath           : "",             // If this empty, default use settings.path + "../plugins/"
+        pluginPath           : "editormd",             // If this empty, default use settings.path + "../plugins/"
         delay                : 300,            // Delay parse markdown to html, Uint : ms
         autoLoadModules      : true,           // Automatic load dependent module files
         watch                : true,
