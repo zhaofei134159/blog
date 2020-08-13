9b3f27f8ca776358ca9ea9007faf3f64d6214a59
zhaofei <zhaofei@51smart.com>
2020-08-13 11:09:18 +0800

    修改

--- application/views/home/public/info_nav.php
+++ application/views/home/public/info_nav.php
@@ -2,10 +2,10 @@
     <div class="panel panel-success">
       <div class="panel-body" style="width:100%;text-align:center;">
           <a href="<?=HOME_URL_HTTP?>user">
-            <h4><i class="fa fa-bars"></i> 用户信息</h4>
+            <h4><i class="fa fa-user"></i> 用户信息</h4>
           </a>
           <a href="<?=HOME_URL_HTTP?>user/blog_setting">
-            <h4><i class="fa fa-warning"></i> 博客配置</h4>
+            <h4><i class="fa fa-cog"></i> 博客配置</h4>
           </a>
           <?php if($user['is_activate']==1){?>
             <?php if(!empty($blog)){?>
@@ -13,10 +13,13 @@
               <h4><i class="fa fa-recycle"></i> 我的博客</h4>
             </a>
             <a href="<?=HOME_URL_HTTP?>user/blog_info">
-              <h4><i class="fa fa-history"></i> 我的分类</h4>
+              <h4><i class="fa fa-flag"></i> 我的分类</h4>
             </a>
             <a href="<?=HOME_URL_HTTP?>user/blog_work_info">
               <h4><i class="fa fa-briefcase"></i> 我的文章</h4>
+            </a>
+            <a href="" target="__block">
+              <h4><i class="fa Statistics"></i> </h4>
             </a>
             <?php }?>
           <?php }?>
