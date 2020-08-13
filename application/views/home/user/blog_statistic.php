fcbe8ebd6f0c40ead94d5a9a8d18167e35044327
zhaofei <zhaofei@51smart.com>
2020-08-13 16:40:45 +0800

    修改

--- application/views/home/user/blog_statistic.php
+++ application/views/home/user/blog_statistic.php
@@ -26,7 +26,6 @@
 <script type="text/javascript" src="<?=HOME_PUBLIC_URL?>js/echarts.min.js"></script>
 <script type="text/javascript"> 
     var myChart = echarts.init(document.getElementById('calendar-eachart'));
-    // 模拟数据
     function getVirtulData(year) {
         year = year || '2017';
         var date = +echarts.number.parseDate(year + '-01-01');
@@ -36,48 +35,46 @@
         for (var time = date; time < end; time += dayTime) {
             data.push([
                 echarts.format.formatTime('yyyy-MM-dd', time),
-                Math.floor(Math.random() * 10000)
+                Math.floor(Math.random() * 10)
             ]);
         }
         return data;
     }
 
     option = {
-        title: {
-            top: 30,
-            left: 'center',
-            text: '2016年某人每天的步数'
-        },
-        tooltip: {},
-        visualMap: {
-            min: 0,
-            max: 10000,
-            type: 'piecewise',
-            orient: 'horizontal',
-            left: 'center',
-            top: 65,
-            textStyle: {
-                color: '#000'
-            }
-        },
-        calendar: {
-            top: 120,
-            left: 30,
-            right: 30,
-            cellSize: ['auto', 13],
-            range: '2016',
-            itemStyle: {
-                borderWidth: 0.5
-            },
-            yearLabel: {show: false}
-        },
-        series: {
-            type: 'heatmap',
-            coordinateSystem: 'calendar',
-            data: getVirtulData(2016)
+    title: {
+        top: 30,
+        left: 'center',
+        text: '2016年某人每天的步数'
+    },
+    visualMap: {
+        min: 0,
+        max: 10,
+        top: 65,
+        color:['#006230','#009B46','#BEE58D'],
+        textStyle: {
+            color: '#ccc'
         }
+    },
+    calendar: {
+        top: 120,
+        left: 30,
+        right: 30,
+        cellSize: ['auto', 13],
+        range: '2016',
+        itemStyle: {
+            borderWidth: 2.5
+        },
+        yearLabel: {show: true}
+    },
+    series: {
+        type: 'heatmap',
+        coordinateSystem: 'calendar',
+        data: getVirtulData(2016)
+    }
     };
-    myChart.setOption(option);
+
+        myChart.setOption(option);
 </script>
 <?php
     $this->load->view('home/public/footer');
