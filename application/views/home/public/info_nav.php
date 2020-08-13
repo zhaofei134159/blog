d6a9ef745ed129ccc9164048dd9ac9ecfbe34c5e
zhaofei <zhaofei@51smart.com>
2020-08-13 11:13:46 +0800

    修改

--- application/views/home/public/info_nav.php
+++ application/views/home/public/info_nav.php
@@ -10,16 +10,16 @@
           <?php if($user['is_activate']==1){?>
             <?php if(!empty($blog)){?>
             <a href="<?=HOME_URL_HTTP?>blog/<?=empty($blog['id'])?'':$blog['id']?>" target="__block">
-              <h4><i class="fa fa-recycle"></i> 我的博客</h4>
+              <h4><i class="fa fa-tripadvisor"></i> 我的博客</h4>
             </a>
             <a href="<?=HOME_URL_HTTP?>user/blog_info">
               <h4><i class="fa fa-flag"></i> 我的分类</h4>
             </a>
             <a href="<?=HOME_URL_HTTP?>user/blog_work_info">
-              <h4><i class="fa fa-briefcase"></i> 我的文章</h4>
+              <h4><i class="fa fa-file-word-o"></i> 我的文章</h4>
             </a>
             <a href="" target="__block">
-              <h4><i class="fa Statistics"></i> </h4>
+              <h4><i class="fa fa-stack-overflow"></i> </h4>
             </a>
             <?php }?>
           <?php }?>
