4a211ea385c7c637ba77e446c2dbdc58fdd48254
zhaofei <zhaofei@51smart.com>
2020-08-31 10:54:13 +0800

    修改

---
+++ tools/dataUpdate.php
@@ -0,0 +1,39 @@
+<?php
+date_default_timezone_set("PRC");
+header("Content-type: text/html; charset=utf-8");
+define('S_PATH', dirname(__FILE__));
+error_reporting(E_ALL);
+
+include_once S_PATH.'/class/database.php';  
+include_once S_PATH.'/class/secretkey.php';  
+include_once S_PATH.'/class/MySql.php';  # mysql
+
+
+# 有几个脚本执行
+$num = exec("ps aux | grep 'getJLInfo.php' | grep -v grep | wc -l");
+if($num>1){
+  exit(date('Y-m-d').' 已经有脚本了');
+}
+
+# 数据库配置
+$db_conf = array(
+    'host' => $db['default']['hostname'],
+    'port' => '3306',
+    'user' => $db['default']['username'],
+    'passwd' => $db['default']['password'],
+    'dbname' => $db['default']['database'],
+);
+
+# mysql
+$mysql = new MMysql($db_conf);
+
+
+# 修改数据
+$sql = "SELECT * from zf_famou_work_info where work_id=9";
+$res = $mysql->doSql($sql);
+
+foreach($res as $key=>$val){
+	$content = explode(' <br> ',$val['content']);
+	var_dump($content);
+	die;
+}
