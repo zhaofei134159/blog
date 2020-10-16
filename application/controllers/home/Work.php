4b5ecfe1858daee0e6083fb036d1c6e3eb910dfb
zhaofei <zhaofei@51smart.com>
2020-10-16 14:42:44 +0800

    图展

--- application/controllers/home/Work.php
+++ application/controllers/home/Work.php
@@ -146,8 +146,7 @@ class Work extends Home_Controller{
 		$get = $this->input->get();
 		$page = !empty($get['page'])?$get['page']:1;
 
-		$imgTag = $this->zf_image_tag_model->select('1');
-		var_dump($imgTag);
+		$imgTag = $this->zf_image_tag_model->select('name!=""');
 		foreach($imgTag as $key=>$val){
 			$imgTag[$key]['count'] = $this->zf_images_model->count('tag='.$val['id'].'');
 		}
