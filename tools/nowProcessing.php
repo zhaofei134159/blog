f349a0aa56c1db223070bfdbb115e22feb8200bd
zhaofei <zhaofei@51smart.com>
2020-12-17 14:27:10 +0800

    进程展示

---
+++ tools/nowProcessing.php
@@ -0,0 +1,21 @@
+<?php
+date_default_timezone_set("PRC");
+header("Content-type: text/html; charset=utf-8");
+define('S_PATH', dirname(__FILE__));
+error_reporting(E_ALL);
+
+include_once S_PATH.'/class/database.php';  
+include_once S_PATH.'/class/MySql.php';  # mysql
+
+$num = exec("ps aux | grep 'nowProcessing.php' | grep -v grep | wc -l");
+if($num>1){
+  exit(date('Y-m-d').' 已经有脚本了');
+}
+
+  $fp = exec('ps -aux');
+  var_dump($fp);
+  $rs = "";
+  while(!feof($fp)){
+    $rs .= fread($fp,1024);
+  }
+  pclose($fp);
