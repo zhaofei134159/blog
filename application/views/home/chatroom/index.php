5bd5e4c77525235df96e80c0f625136b9bebda41
zhaofei <zhaofei@51smart.com>
2021-02-24 14:17:24 +0800

    聊天室

--- application/views/home/chatroom/index.php
+++ application/views/home/chatroom/index.php
@@ -3,23 +3,20 @@
 ?>
 
 <style type="text/css">
-    #chatroom{width:680px;height:550px;margin:100px auto;background:red;}
-    #chatroominput{width:680px;height:150px;margin:100px auto;background:green;}
+    #chatroom{width:100%;height:550px;background:red;}
+    #chatroominput{width:100%;height:150px;background:green;}
 </style>
 <!-- MENU SECTION END-->
 <div class="content-wrapper" style="min-height:600px;">
     <div class="container">
 
         <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="col-md-10 col-sm-10 col-xs-10">
  				<div class="panel panel-default">
                     <div class="panel-heading">
                        聊天室
                     </div>
                     <div class="panel-body text-center recent-users-sec" id="chatroom">
-                        
-                    </div>
-                    <div class="panel-body text-center recent-users-sec" id="chatroominput">
                         
                     </div>
      			</div>
