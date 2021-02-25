ea83bc6e3b3380650936ea7c8f3cc9657c7ebf87
zhaofei <zhaofei@51smart.com>
2021-02-25 20:00:09 +0800

    聊天室

--- application/views/home/chatroom/index.php
+++ application/views/home/chatroom/index.php
@@ -58,10 +58,8 @@
 
         // 关闭网页 断开连接
         window.onbeforeunload=function (){
-            if(event.clientX > document.body.clientWidth || event.clientY < 0 ||event.altKey || event.ctrlKey){
-                dis();
-                alert(123213213);
-            }
+            dis();
+            alert(123213213);
         }
     })
     function socket_link(){
