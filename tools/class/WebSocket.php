d186a8233410d35854ffe6823b0e9a334229e976
zhaofei <zhaofei@51smart.com>
2021-02-26 10:19:12 +0800

    聊天室

--- tools/class/WebSocket.php
+++ tools/class/WebSocket.php
@@ -73,7 +73,7 @@ class WebSocket{
 		          	$this->userreturn('in',$usermsg);
 		        }else{
 		        	// $len = 0 为正常退出 -1 为已经执行了，只不过失败了
-		          	$len = socket_recv($sign,$buffer,4096, 0);
+		          	$len = socket_recv($sign,$buffer,4096, 0x2);
 		          	$userid = $this->search($sign);
 		          	$user = $this->users[$userid];
 		          	if($len<7){
