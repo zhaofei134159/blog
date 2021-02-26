f4fe4232852aace37d0d48fbae0104c5302304ab
zhaofei <zhaofei@51smart.com>
2021-02-26 10:42:36 +0800

    聊天室

--- tools/class/WebSocket.php
+++ tools/class/WebSocket.php
@@ -73,13 +73,7 @@ class WebSocket{
 		          	$this->userreturn('in',$usermsg);
 		        }else{
 		        	// $len = 0 为正常退出 -1 为已经执行了，只不过失败了
-		          	$len = socket_recv($sign,$buffer,4096, 0);
-		          	$buffer = '';
-		          	while(0 != socket_recv($sign, $out, 1024, 0)){
-					    if($out != null){
-					        $buffer .= $out;
-					    }
-					}
+		          	$len = socket_recv($sign, $buffer, 8192, 0);
 		          	$userid = $this->search($sign);
 		          	$user = $this->users[$userid];
 		          	if($len<7){
