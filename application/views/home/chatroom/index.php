d501010e93781a93dc584079418a74c10141e54a
zhaofei <zhaofei@51smart.com>
2021-02-26 15:54:56 +0800

    聊天室

--- application/views/home/chatroom/index.php
+++ application/views/home/chatroom/index.php
@@ -3,7 +3,7 @@
 ?>
 
 <style type="text/css">
-    #chatroom{width:100%;height:450px;padding:20px 30px; border-bottom:solid 1px #ccc;}
+    #chatroom{width:100%;height:450px;padding:20px 30px; border-bottom:solid 1px #ccc;overflow-x: none;overflow-y: auto;}
     #chatroominput{width:100%;text-align:left;}
     #contInput{width: 90%;height: 45px;border: 1px #ccc solid; border-radius: 5px;}
     #contButton{width: 60px;height: 45px;margin-left: 15px;background: #fff;border: solid 1px #ccc;color: black;border-radius: 5px;}
