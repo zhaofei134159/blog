76cb9ff608a25adc7a8e2e5a32185dd45f7d6f5d
zhaofei <13700530350@163.com>
2024-07-29 00:06:30 +0800

    紫微斗数

--- application/controllers/interface/ZiweiInfoapp.php
+++ application/controllers/interface/ZiweiInfoapp.php
@@ -30,121 +30,24 @@ class ZiweiInfoapp extends Home_Controller{
 		outputJson($data);
 	}
 
-	public function lookWork(){
-		$blogId = $this->blogId;
-		$workId = $_GET['workId'];
+	public function infoDetail(){
+		$itemId = $_POST['itemId'];
+		$itemType = $_POST['itemType'];
 
-		$Htmlrepair = new Htmlrepair();
+		$table = '';
+		if($itemType == 'palace'){
+			$table = 'ziwei_palace_info';
+		}
 
-		$where = 'blog_id='.$blogId.' and is_del=0';
-		$where .= ' and id='.$workId;
-		$work = $this->zf_work_model->select_one($where);
+		$palaceInfo = $this->fortune->query("SELECT * from {$table} WHERE id={$itemId} LIMIT 20");
 
-		$work['desc'] = $Htmlrepair->fix_html_tags($work['desc']);
-		$work['desc'] = str_replace('"=""','',$work['desc']);
-
-
-
-		$work['user'] = $this->zf_user_model->select_one('id='.$work['uid']);
-		
 		//点击量
-		$work['browse_num'] += 1;
-		$this->zf_work_model->update(array('browse_num'=>$work['browse_num']),$where);
-
-
-		//标签
-		$tag_where = 'blog_id='.$blogId.'  and is_del=0';
-		if(!empty($work['tag_ids'])){
-			$tag_where .= ' and id in('.$work['tag_ids'].')';
-		}
-		$tags = $this->zf_tag_model->select($tag_where);
-
-		//分类
-		$cates = $this->zf_cate_model->get_list('blog_id='.$blogId.' and is_del=0','*','',5,0);
-
-		//相关文章
-		$relevant = $this->work_relevant($work['tag_ids'],$blogId,$workId);
-		
+		// $work['browse_num'] += 1;
+		// $this->zf_work_model->update(array('browse_num'=>$work['browse_num']),$where);
 
 		$data = array(
-				'cates'=>$cates,
-				'tags'=>$tags,
-				'work'=>$work,
-				'relevant'=>$relevant
+				'palaceInfo'=>$palaceInfo,
 			);
-
-		echo json_encode($data);
+		outputJson($data);
 	}
-
-	//相关文章
-	function work_relevant($tag_ids,$id,$wid){
-		$data = array();
-		if(empty($tag_ids)){
-			return $data;
-		}
-
-		$relevant_where = 'blog_id='.$id.' and id != '.$wid.' and is_del=0 and (';
-		$tag_idarr=explode(',',$tag_ids);
-		$where = '';
-		foreach($tag_idarr as $key=>$val){
-			$where .= ' FIND_IN_SET('.$val.',tag_ids) or';
-		}
-		$relevant_where .= trim($where,'or');
-
-		$relevant_where .= ')';
-
-		$relevant = $this->zf_work_model->get_list($relevant_where,'*','',5,0);
-		
-		return $relevant;
-	}
-
-	function tagList(){
-		
-		$tags = $this->zf_tag_model->get_list('is_del=0 and blog_id='.$this->blogId,'*','ctime desc',40,0);
-
-		$data = array(
-			'tags'=>$tags,
-		);
-		echo json_encode($data);
-	}
-
-	function search(){
-		$blogId = $this->blogId;
-		$search = $_GET['search'];
-
-		$where = 'blog_id='.$blogId.' and is_del=0';
-		$where .= ' and title like "%'.$search.'%"';
-		$where .= ' and desc like "%'.$search.'%"';
-		$works = $this->zf_work_model->select($where);
-
-		$data = array(
-			'works'=>$works,
-		);
-		echo json_encode($data);
-	}
-
-	function tagSearch(){
-		$blogId = $this->blogId;
-		$tagId = $_GET['tagId'];
-
-		$tag_where = '';
-		$where = 'blog_id='.$blogId.' and is_del=0';
-		if(!empty($tagId)){
-			$tag_idarr=explode(',',trim($tagId,','));
-			$where .= " and (";
-			foreach($tag_idarr as $key=>$val){
-				$tag_where .= ' FIND_IN_SET('.$val.',tag_ids) or';
-			}
-			$where .= trim($tag_where,'or');
-			$where .= ')';
-		}
-
-		$works = $this->zf_work_model->select($where);
-
-		$data = array(
-			'works'=>$works,
-		);
-		echo json_encode($data);
-	}
-	
 }
