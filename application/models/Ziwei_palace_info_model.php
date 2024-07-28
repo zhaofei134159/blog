0109138e7cb7b8cd1ed486d03e8a5258e8809d62
zhaofei <13700530350@163.com>
2024-07-28 23:02:10 +0800

    紫微斗数

--- application/models/Ziwei_palace_info_model.php
+++ application/models/Ziwei_palace_info_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Ziwei_palace_info extends Myf_model {
+class Ziwei_palace_info_model extends Myf_model {
 
 	public function __construct(){
 		parent::__construct();
